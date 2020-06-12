62ccc7588850b942272e831f721628a468d2e911
Shaneciora <33504380+Shaneciora@users.noreply.github.com>
2020-06-11 19:53:39 -0700

    flask: typo

--- flask/run.py
+++ flask/run.py
@@ -13,4 +13,4 @@ def get_bot_response():
     return json.dumps(resp)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
