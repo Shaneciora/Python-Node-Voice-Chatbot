a6d41228cd5b92bd3cdf2c049b909ab0a28ce6c9
Shaneciora <33504380+Shaneciora@users.noreply.github.com>
2020-06-11 19:32:47 -0700

    run: config'd server: setup axios (just a test)

--- flask/run.py
+++ flask/run.py
@@ -1,1 +1,18 @@
-#initialize
+
+
+from flask import Flask, render_template, request
+import json
+
+app = Flask(__name__)
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    resp = {
+        'message': userText,
+    }
+    return json.dumps(resp)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
--- nodejs/server.js
+++ nodejs/server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var axios = require('axios');
+
 var app = express();
 
 app.set('view engine', 'pug');
@@ -6,7 +8,19 @@ app.set('view engine', 'pug');
 // serve static files from the `public` folder
 app.use(express.static(__dirname + '/public'));
 
+
 app.get('/', function (req, res) {
+	axios({
+  	method: 'get',
+  	url: 'http://localhost:5000/get?msg=test1',
+  	responseType: 'stream'
+	})
+  .then(function (response) {
+		var data = JSON.parse(response.data);
+		console.log(data);
+    //response.data.pipe(fs.createWriteStream('ada_lovelace.jpg'))
+  });
+
 	res.render('index');
 })
 
