35c62fe6200e894bf6167074c094422eef358163
Shane Ciora <sciora@jarvis.lan>
2020-06-11 18:27:33 -0700

    setting up python-shell

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
+import {PythonShell} from 'python-shell';
+
+
 var express = require('express');
 var app = express();
 
+let options = {
+  mode: 'text',
+  pythonPath: 'scripts/py',
+  pythonOptions: ['-u'], // get print results in real-time
+  scriptPath: 'path/to/my/scripts',
+  args: ['how are you?']
+};
+
+
 app.set('view engine', 'pug');
+
+
+
+
+PythonShell.run('bot.py', options, function (err, results) {
+  if (err) throw err;
+  // results is an array consisting of messages collected during execution
+  console.log('results: %j', results);
+});
 
 
 // serve static files from the `public` folder
