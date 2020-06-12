12c7b4b83843845f6feab3327630e4f927769c59
Shaneciora <33504380+Shaneciora@users.noreply.github.com>
2020-06-11 19:58:15 -0700

    flask: fixed db threading

--- flask/bot.py
+++ flask/bot.py
@@ -5,6 +5,7 @@ from collections import Counter
 from string import punctuation
 from math import sqrt
 
+
 # initialize the connection to the database
 connection = sqlite3.connect('chatbot.sqlite')
 cursor = connection.cursor()
@@ -21,19 +22,19 @@ for create_table_request in create_table_request_list:
     except:
         pass
 
-def get_id(entityName, text):
+def get_id(c,entityName, text):
     """Retrieve an entity's unique ID from the database, given its associated text.
     If the row is not already present, it is inserted.
     The entity can either be a sentence or a word."""
     tableName = entityName + 's'
     columnName = entityName
-    cursor.execute('SELECT rowid FROM ' + tableName + ' WHERE ' + columnName + ' = ?', (text,))
-    row = cursor.fetchone()
+    c.execute('SELECT rowid FROM ' + tableName + ' WHERE ' + columnName + ' = ?', (text,))
+    row = c.fetchone()
     if row:
         return row[0]
     else:
-        cursor.execute('INSERT INTO ' + tableName + ' (' + columnName + ') VALUES (?)', (text,))
-        return cursor.lastrowid
+        c.execute('INSERT INTO ' + tableName + ' (' + columnName + ') VALUES (?)', (text,))
+        return c.lastrowid
 
 def get_words(text):
     """Retrieve the words present in a given string of text.
@@ -45,6 +46,9 @@ def get_words(text):
     return Counter(wordsList).items()
 
 def runbot(text):
+    conn = sqlite3.connect('chatbot.sqlite')
+    c = conn.cursor()
+
     B = ''
     H = text
     #print("User: " + H)
@@ -55,34 +59,34 @@ def runbot(text):
     # store the association between the bot's message words and the user's response
     words = get_words(B)
     words_length = sum([n * len(word) for word, n in words])
-    sentence_id = get_id('sentence', H)
+    sentence_id = get_id(c, 'sentence', H)
     for word, n in words:
-        word_id = get_id('word', word)
+        word_id = get_id(c, 'word', word)
         weight = sqrt(n / float(words_length))
-        cursor.execute('INSERT INTO associations VALUES (?, ?, ?)', (word_id, sentence_id, weight))
+        c.execute('INSERT INTO associations VALUES (?, ?, ?)', (word_id, sentence_id, weight))
     connection.commit()
 
 
     # retrieve the most likely answer from the database
-    cursor.execute('CREATE TEMPORARY TABLE results(sentence_id INT, sentence TEXT, weight REAL)')
+    c.execute('CREATE TEMPORARY TABLE results(sentence_id INT, sentence TEXT, weight REAL)')
     words = get_words(H)
     words_length = sum([n * len(word) for word, n in words])
     for word, n in words:
         weight = sqrt(n / float(words_length))
-        cursor.execute('INSERT INTO results SELECT associations.sentence_id, sentences.sentence, ?*associations.weight/(4+sentences.used) FROM words INNER JOIN associations ON associations.word_id=words.rowid INNER JOIN sentences ON sentences.rowid=associations.sentence_id WHERE words.word=?', (weight, word,))
+        c.execute('INSERT INTO results SELECT associations.sentence_id, sentences.sentence, ?*associations.weight/(4+sentences.used) FROM words INNER JOIN associations ON associations.word_id=words.rowid INNER JOIN sentences ON sentences.rowid=associations.sentence_id WHERE words.word=?', (weight, word,))
 
 
     # if matches were found, give the best one
-    cursor.execute('SELECT sentence_id, sentence, SUM(weight) AS sum_weight FROM results GROUP BY sentence_id ORDER BY sum_weight DESC LIMIT 1')
-    row = cursor.fetchone()
-    cursor.execute('DROP TABLE results')
+    c.execute('SELECT sentence_id, sentence, SUM(weight) AS sum_weight FROM results GROUP BY sentence_id ORDER BY sum_weight DESC LIMIT 1')
+    row = c.fetchone()
+    c.execute('DROP TABLE results')
 
 
     # otherwise, just randomly pick one of the least used sentences
     if row is None:
-        cursor.execute('SELECT rowid, sentence FROM sentences WHERE used = (SELECT MIN(used) FROM sentences) ORDER BY RANDOM() LIMIT 1')
-        row = cursor.fetchone()
+        c.execute('SELECT rowid, sentence FROM sentences WHERE used = (SELECT MIN(used) FROM sentences) ORDER BY RANDOM() LIMIT 1')
+        row = c.fetchone()
     # tell the database the sentence has been used once more, and prepare the sentence
     B = row[1]
-    cursor.execute('UPDATE sentences SET used=used+1 WHERE rowid=?', (row[0],))
+    c.execute('UPDATE sentences SET used=used+1 WHERE rowid=?', (row[0],))
     return B
