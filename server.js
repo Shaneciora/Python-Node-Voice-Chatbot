905c9563650c6bb31e4ba58a4c6c6fa98685dc99
Shaneciora <33504380+Shaneciora@users.noreply.github.com>
2020-06-11 18:11:48 -0700

    server: / now renders index

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-	res.send('Hello Wordld');
+	res.render('index');
 })
 
 var server = app.listen(5000, function () {
