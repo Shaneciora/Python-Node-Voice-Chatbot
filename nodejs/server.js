50d1b34747c91805901d548a4d3605ddb9cb2624
Shaneciora <33504380+Shaneciora@users.noreply.github.com>
2020-06-11 19:05:52 -0700

    all: configuring flask and nodejs (moving things around)

---
+++ nodejs/server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'pug');
+
+// serve static files from the `public` folder
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+	res.render('index');
+})
+
+var server = app.listen(5000, function () {
+   var host = server.address().address
+   var port = server.address().port
+
+   console.log("Example app listening at http://%s:%s", host, port)
+})
